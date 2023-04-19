d52855ecd7088ab64cb961f476b05f4a08dc0417
umaumax <umauma7083@gmail.com>
2023-04-19 22:56:12 +0900

    Change scroll behavior to smooth

--- script.js
+++ script.js
@@ -5,17 +5,41 @@
             console.log("message:", message);
 
             if (message.type === "up") {
-                window.scrollBy(0, -100);
+                window.scrollBy({
+                    top: -100,
+                    left: 0,
+                    behavior: 'smooth'
+                });
             } else if (message.type === "down") {
-                window.scrollBy(0, 100);
+                window.scrollBy({
+                    top: 100,
+                    left: 0,
+                    behavior: 'smooth'
+                });
             } else if (message.type === "half page up") {
-                window.scrollBy(0, -window.innerHeight / 2);
+                window.scrollBy({
+                    top: -window.innerHeight / 2,
+                    left: 0,
+                    behavior: 'smooth'
+                });
             } else if (message.type === "half page down") {
-                window.scrollBy(0, window.innerHeight / 2);
+                window.scrollBy({
+                    top: window.innerHeight / 2,
+                    left: 0,
+                    behavior: 'smooth'
+                });
             } else if (message.type === "page up") {
-                window.scrollBy(0, -window.innerHeight);
+                window.scrollBy({
+                    top: -window.innerHeight,
+                    left: 0,
+                    behavior: 'smooth'
+                });
             } else if (message.type === "page down") {
-                window.scrollBy(0, window.innerHeight);
+                window.scrollBy({
+                    top: window.innerHeight,
+                    left: 0,
+                    behavior: 'smooth'
+                });
             } else {
                 console.log("invalid message type:", message.type);
             }
